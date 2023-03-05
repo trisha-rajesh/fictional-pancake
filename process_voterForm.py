d1921c14dd15e9b26b06dd1c85e86eaf87442336
Trisha Rajesh <trisharajesh@Trishas-MacBook-Pro.local>
2023-03-04 20:46:32 -0600

    print stuff

--- process_voterForm.py
+++ process_voterForm.py
@@ -12,3 +12,21 @@ city = form.getvalue('city')
 
 # Get the candidate values from the form as a list
 candidates = form.getlist('candidates')
+
+print("Content-type:text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>Voter Form Submission</title>")
+print("</head>")
+print("<body>")
+print("<h1>Thank you for submitting your voter form!</h1>")
+print("<p>Name: " + str(name) + "</p>")
+print("<p>Age: " + str(age) + "</p>")
+print("<p>Gender: " + str(gender) + "</p>")
+print("<p>City: " + str(city) + "</p>")
+print("<p>Candidates:")
+for candidate in candidates:
+    print("<br>" + str(candidate))
+print("</p>")
+print("</body>")
+print("</html>")
